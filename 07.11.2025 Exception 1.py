91785c42dadaefc76437e3bae2c0d7ff110dcb23
Manish Bilgaye <manishbilgaye@gmail.com>
2025-11-07 13:06:53 +0100

    Update string length validation logic in Exception 1 file

--- 07.11.2025 Exception 1.py
+++ 07.11.2025 Exception 1.py
@@ -1,6 +1,21 @@
-num = 0
-while num != 100:
-    num = int(input("Enter the number: "))
-    print("Checking your number: ")
+def main():
+    num = 0
 
-print("You have entered correct number 100!")
+    while num != 100:
+        num = int(input("Enter the number: "))
+        print("Checking your number: ")
+    print("You have entered correct number 100!")
+
+if __name__ =="__main__":
+    main()
+
+def main():
+    s = input("Enter the string characters: ")
+
+    while len(s) < 20:
+        print("Check the length of the string....")
+        s = input("Enter the string characters again")
+    print("You got it!")
+
+if __name__ == "__main__":
+    main()
